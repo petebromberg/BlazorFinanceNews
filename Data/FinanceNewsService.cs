00171f226fcc72ffb701cdf44f7e063398b17a45
petebromberg <pbromberg@gmail.com>
2021-03-28 14:31:35 -0400

    Fix Rendermode to Server

--- Data/FinanceNewsService.cs
+++ Data/FinanceNewsService.cs
@@ -17,8 +17,14 @@ namespace BlazorFinanceNews.Data
 
             var requestUri = new Uri("https://yahoo-finance15.p.rapidapi.com/api/yahoo/ne/news");
             var client = new HttpClient();
+
+            // IMPORTANT! Go here first to get your own x-rapidapi-key: https://rapidapi.com/apidatacenter-api-data/api/yahoo-finance15
+
             client.DefaultRequestHeaders.Add("x-rapidapi-key", "c9fa1e73ddmshe7b02f8018130b4p164618jsn88999a8a0cad");
+            
             client.DefaultRequestHeaders.Add("x-rapidapi-host", "yahoo-finance15.p.rapidapi.com");
+
+            
 
             CancellationTokenSource source = new CancellationTokenSource();
             CancellationToken token = source.Token;
